2ba99885c9d6394778c7f3aaeb54f757fba9f752
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-09 16:53:18 -0300

    Agrego configMiddleware

--- src/Mercurio.js
+++ src/Mercurio.js
@@ -2,9 +2,22 @@ import { Controller, Series } from './Controllers';
 
 export class Mercurio extends Controller {
     
+    constructor(express, config){
+        super(express);
+        this.config = config;
+
+        this.configMiddleware = this.configMiddleware.bind(this);
+    }
+
     applyMiddleware(app) {
+        app.use(this.configMiddleware);
         app.use('/series', this.series);
         return app;
+    }
+
+    configMiddleware(req, res, next){
+        res.locals.config = this.config;
+        next();
     }
 
     listen(...args){
