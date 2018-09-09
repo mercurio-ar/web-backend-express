93cb6cba8d2f9d768296cd828f72438f7663cb63
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-09 16:52:49 -0300

    Agrego search handler

--- src/Controllers/Series/Search.js
+++ src/Controllers/Series/Search.js
@@ -1,11 +1,39 @@
-import { Controller } from '../Controller';
+import {
+    Controller
+} from '../Controller';
+import {
+    seriesSearchServiceConfigSelector
+} from '../../Selectors';
+import {
+    SeriesSearchService
+} from '../../Services';
 
 export class Search extends Controller {
 
-    applyMiddleware(router){
-        router.get('/', function(req, res) {
-            res.json({results: 'sarasa'});
-        });
+    constructor(...args){
+        super(...args);
+
+        this.searchServiceMiddleware = this.searchServiceMiddleware.bind(this);
+        this.searchHandler = this.searchHandler.bind(this);
+    }
+
+    applyMiddleware(router) {
+        router.use(this.searchServiceMiddleware);
+        router.get('/', this.searchHandler);
         return router;
+    }
+
+    searchServiceMiddleware(req, res, next) {
+        const searchServiceConfig = seriesSearchServiceConfigSelector(res);
+        res.locals.searchService = new SeriesSearchService(searchServiceConfig);
+        next();
+    }
+
+    searchHandler(req, res) {
+        res.locals.searchService.search(req.query).then(result => {
+            res.json(
+                result
+            );
+        });
     }
 }
