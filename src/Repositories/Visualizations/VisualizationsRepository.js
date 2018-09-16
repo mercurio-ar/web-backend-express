3d4a49ee4ea72cdcc90eeb8b0de9f2a8c23c7af2
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-16 17:20:48 -0300

    Agrego visualizationsRepository

---
+++ src/Repositories/Visualizations/VisualizationsRepository.js
@@ -0,0 +1,31 @@
+import {
+    VisualizationSchema
+} from '../../Schemas';
+
+export class VisualizationsRepository {
+
+    constructor({
+        mongoose,
+        uri
+    }) {
+        this.mongoose = mongoose;
+        this.uri = uri;
+    }
+
+    connect() {
+        return this.mongoose.connect(this.uri, {
+            useNewUrlParser: true,
+        }).then(() => {
+            this.model = this.mongoose.model('Visualizations', VisualizationSchema);
+            return arguments;
+        });
+    }
+
+    disconnect() {
+        this.mongoose.disconnect();
+    }
+
+    getVisualizations() {
+        return this.model.find().exec();
+    }
+}
