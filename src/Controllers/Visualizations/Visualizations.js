bdcc9d809dbd4c0abd3c43178fa0a8df1384e969
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-16 17:17:21 -0300

    Agrego visualization service y visualization repository middlewares

--- src/Controllers/Visualizations/Visualizations.js
+++ src/Controllers/Visualizations/Visualizations.js
@@ -1,25 +1,30 @@
 import { Controller } from '../Controller';
 import { VisualizationsService } from '../../Services';
+import { VisualizationsRepository } from '../../Repositories';
+import { mongoDbConfigSelector } from '../../Selectors';
 
 export class Visualizations extends Controller {
 
     constructor(...args){
         super(...args);
 
+        this.visualizationsRepositoryMiddleware = this.visualizationsRepositoryMiddleware.bind(this);
         this.visualizationsServiceMiddleware = this.visualizationsServiceMiddleware.bind(this);
         this.getVisualizationsHandler = this.getVisualizationsHandler.bind(this);
     }
 
     applyMiddleware(router) {
+        router.use(this.visualizationsRepositoryMiddleware);
         router.use(this.visualizationsServiceMiddleware);
         router.get('/', this.getVisualizationsHandler);
         return router;
     }
 
-    getVisualizationsHandler(req, res){
-        res.json(
-            this.visualizationsServiceSelector(res).getVisualizations()
-        );
+    getVisualizationsHandler(req, res, next){
+        this.visualizationsServiceSelector(res)
+            .getVisualizations()
+            .then(res.json)
+            .catch(next);
     }
 
     visualizationsServiceSelector(res){
@@ -27,7 +32,24 @@ export class Visualizations extends Controller {
     }
 
     visualizationsServiceMiddleware(req, res, next){
-        res.locals.visualizationsService = new VisualizationsService();
+        res.locals.visualizationsService = new VisualizationsService(
+            this.visualizationsRepositorySelector(res)
+        );
         next();
+    }
+
+    visualizationsRepositorySelector(res){
+        return res.locals.visualizationRepository;
+    }
+
+    visualizationsRepositoryMiddleware(req, res, next){
+        const mongoConfig = mongoDbConfigSelector(res);
+        const visualizationsRepository = new VisualizationsRepository(mongoConfig);
+        visualizationsRepository.connect().then((repository) => {
+            res.locals.visualizationRepository = repository;
+            next();
+        }).catch(
+            next
+        );
     }
 }
