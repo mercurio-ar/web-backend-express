dcccefbf87fff62ce81da19ff9cdb4e7d767bdf6
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-16 17:11:22 -0300

    agrego config de mongo

--- config/development.js
+++ config/development.js
@@ -1,9 +1,15 @@
 import Axios from 'axios';
-
+import mongoose from 'mongoose';
 
 export default {
     port: 3001,
     cors: true,
+    db: {
+        mongo: {
+            mongoose,
+            uri: 'mongodb://root:example@localhost:8081/mercurio'
+        }
+    },
     series:{
         services:{
             search:{
