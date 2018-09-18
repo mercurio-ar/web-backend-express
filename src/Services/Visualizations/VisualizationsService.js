ce7fe22d14b0f1bd10c77992a88d043087754fc9
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-18 11:00:42 -0300

    Agrego metodo create visualization with series a visualizationsservice

--- src/Services/Visualizations/VisualizationsService.js
+++ src/Services/Visualizations/VisualizationsService.js
@@ -1,10 +1,27 @@
 export class VisualizationsService {
 
-    constructor(visualizationsRepository){
+    constructor(visualizationsRepository) {
         this.visualizationsRepository = visualizationsRepository;
     }
 
     getVisualizations() {
         return this.visualizationsRepository.getVisualizations();
+    }
+
+    createVisualizationWithSeries(series) {
+        this._validateSeries(series);
+        return this.visualizationsRepository
+            .createVisualization(this._defaultName(), series);
+    }
+
+    _defaultName() {
+        return 'New Visualization';
+    }
+
+    _validateSeries(series) {
+        const invalid = !!series.find(serie => typeof serie != 'string');
+        if (invalid) {
+            throw Error('Expected Array of string to create visualization');
+        }
     }
 }
