525c084fd41699a43dcb286f9749cd753425e7e8
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-22 16:51:42 -0300

    Added serialization to visualization query

--- src/Controllers/Visualizations/Query/VisualizationsQueryStack.js
+++ src/Controllers/Visualizations/Query/VisualizationsQueryStack.js
@@ -1,18 +1,26 @@
-import { Controller } from '../../Controller';
-import { visualizationsServiceSelector } from '../../../Selectors';
+import {
+    Controller,
+} from '../../Controller';
+import {
+    visualizationsServiceSelector,
+    visualizationsAdapterSelector,
+} from '../../../Selectors';
 
-export class VisualizationsQueryStack extends Controller{
+export class VisualizationsQueryStack extends Controller {
 
-    applyMiddleware(router){
+    applyMiddleware(router) {
         router.get('/', this.getVisualizationsHandler);
         return router;
     }
 
-    getVisualizationsHandler(req, res, next){
+    getVisualizationsHandler(req, res, next) {
         visualizationsServiceSelector(res)
             .getVisualizations()
             .then(result => {
-                res.json(result);
+                res.json(
+                    visualizationsAdapterSelector(res)
+                        .serialize(result)
+                );
             })
             .catch(next);
     }
