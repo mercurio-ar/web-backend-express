90440d6a64d08b70fcb4e8c4ee8eb0ac7e0d26f0
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-22 16:48:12 -0300

    Implemented Delete visualization command

--- src/Controllers/Visualizations/Command/VisualizationsDeleteCommand.js
+++ src/Controllers/Visualizations/Command/VisualizationsDeleteCommand.js
@@ -1,8 +1,24 @@
-import { Controller } from '../../Controller';
+import {
+    Controller
+} from '../../Controller';
+import {
+    visualizationsServiceSelector,
+    visualizationsAdapterSelector
+} from '../../../Selectors';
 
 export class VisualizationsDeleteCommand extends Controller {
 
-    applyMiddleware(router){
+    applyMiddleware(router) {
+        router.use(this.deleteVisualizationsHandler);
         return router;
+    }
+    
+    deleteVisualizationsHandler(req, res) {
+        const visualizationIds = visualizationsAdapterSelector(res).parse(req.body).visualizationsIds();
+        visualizationsServiceSelector(res)
+            .deleteVisualizations(visualizationIds)
+            .then(() => {
+                res.status(204).send();
+            });
     }
 }
