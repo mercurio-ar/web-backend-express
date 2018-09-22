ae88ceddde5b84216ba59211f77092a88b05259b
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-22 16:46:23 -0300

    Added: serialization step when creating visualizations

--- src/Controllers/Visualizations/Command/VisualizationsCreateCommand.js
+++ src/Controllers/Visualizations/Command/VisualizationsCreateCommand.js
@@ -2,32 +2,25 @@ import {
     Controller
 } from '../../Controller';
 import {
-    visualizationsSerializerSelector,
+    visualizationsAdapterSelector,
     visualizationsServiceSelector
 } from '../../../Selectors';
-import {
-    VisualizationSerializer
-} from '../../../Serializers';
 
 export class VisualizationsCreateCommand extends Controller {
 
     applyMiddleware(router) {
-        router.use(this.serializerMiddleware);
         router.use(this.createVisualizationHandler);
         return router;
     }
 
-    serializerMiddleware(req, res, next) {
-        res.locals.visualizationSerializer = new VisualizationSerializer();
-        next();
-    }
-
     createVisualizationHandler(req, res) {
-        const seriesIds = visualizationsSerializerSelector(res).parse(req.body).seriesIds();
+        const seriesIds = visualizationsAdapterSelector(res).parse(req.body).seriesIds();
         visualizationsServiceSelector(res)
             .createVisualizationWithSeries(seriesIds)
             .then(vis => {
-                res.json(vis);
+                res.json(
+                    visualizationsAdapterSelector(res).serialize(vis)
+                );
             });
     }
 }
