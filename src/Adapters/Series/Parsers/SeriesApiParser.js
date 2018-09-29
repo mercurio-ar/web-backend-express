f6d1b4ed05f8668e0c74371cdb656d0523b8d9b4
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-29 12:17:33 -0300

    Fix: series displayName is now parsed

--- src/Adapters/Series/Parsers/SeriesApiParser.js
+++ src/Adapters/Series/Parsers/SeriesApiParser.js
@@ -10,6 +10,7 @@ export class SeriesApiParser {
     static parse(seriesApiResponse) {
         return seriesApiResponse.meta.map((meta, index) => ({
             id: meta.field && meta.field.id,
+            displayName: meta.field && meta.field.description,
             points: seriesApiResponse.data.map((point) => ({
                 x: point[0],
                 y: point[index]
